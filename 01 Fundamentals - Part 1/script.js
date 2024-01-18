f76c65dd32f628f920899a3d2f2f96439f4bc3a1
RhysPrograms <rhysatkinson19@gmail.com>
2024-01-18 20:41:27 +0000

    UDMY-10: Values and Variables

--- 01 Fundamentals - Part 1/script.js
+++ 01 Fundamentals - Part 1/script.js
@@ -1,5 +1,15 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
