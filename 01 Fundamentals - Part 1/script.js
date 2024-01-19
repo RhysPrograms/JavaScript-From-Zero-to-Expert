20e9b1c1f76bb6f981236963346ffe81cadc5837
RhysPrograms <rhysatkinson19@gmail.com>
2024-01-19 08:59:04 +0000

    UDMY-10: Data Types

--- 01 Fundamentals - Part 1/script.js
+++ 01 Fundamentals - Part 1/script.js
@@ -1,15 +1,36 @@
+/* 
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
 let firstName = "Jonas";
 console.log(firstName);
 
+// Variable Name Conventions
 let jonas_matilda = "JM";
 let $function = 27;
-
 let PI = 3.1415;
-
 let myFirstJob = "Programmer";
 let myCurrentJob = "Teacher";
 
 console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+console.log(typeof 23);
+console.log(typeof "Rhys");
+
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null); // This will return 'Object', as opposed to 'null' and it is regarded as a Legacy Bug
