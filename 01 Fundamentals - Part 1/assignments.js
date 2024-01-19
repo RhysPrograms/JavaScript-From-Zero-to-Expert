60f9f8fc87a225cb6169de4eabfb8df551290b71
RhysPrograms <rhysatkinson19@gmail.com>
2024-01-19 09:03:33 +0000

    UDMY-10: Data Types (Assignment)

--- 01 Fundamentals - Part 1/assignments.js
+++ 01 Fundamentals - Part 1/assignments.js
@@ -1,6 +1,16 @@
+// Values and Variables
 let country = "England";
 let continent = "Europe";
 let population = "70 Million";
+
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let isIsland = true;
+let variable;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
