4b2df1b9a59e58ed5b811b8fcf7f6d7c8301a4e5
RhysPrograms <rhysatkinson19@gmail.com>
2024-01-19 09:48:45 +0000

    UDMY-10: let, const and var

--- 01 Fundamentals - Part 1/script.js
+++ 01 Fundamentals - Part 1/script.js
@@ -1,4 +1,6 @@
 /* 
+
+// Values and Variables
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
@@ -13,8 +15,8 @@ let myFirstJob = "Programmer";
 let myCurrentJob = "Teacher";
 
 console.log(myFirstJob);
-*/
 
+// Data Types
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
@@ -33,4 +35,19 @@ year = 1991;
 console.log(year);
 console.log(typeof year);
 
-console.log(typeof null); // This will return 'Object', as opposed to 'null' and it is regarded as a Legacy Bug
+console.log(typeof null); // this will return 'Object', as opposed to 'null' and it is regarded as a Legacy Bug
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991; // the const keyword makes an immutable variable (content cannot be changed)
+birthYear = 1990;
+
+const job; // const variables must be assigned a value when they are declared
+
+var jobTitle = "Test Specialist"; // before the advent of ES6, var declarations ruled. There are issues associated with variables declared with var, that is why it was necessary for new ways to declare variables
+
+lastName = "Schmedtmann"; // Whilst this will work, do NOT write a variable without declaring it
+console.log(lastName);
+
