41a66ef092bfab4f9a1f2f3d326c7f17d0465a39
RhysPrograms <rhysatkinson19@gmail.com>
2024-01-22 11:37:37 +0000

    UDMY-10: Challenge #2

--- 01 Fundamentals - Part 1/script.js
+++ 01 Fundamentals - Part 1/script.js
@@ -150,7 +150,7 @@ console.log("This is a string with \nmultiple \nlines");
 console.log(`This is a string with
 multiple
 lines`);
-*/
+
 
 // ************************************     //
 // TAKING DECISIONS: if / else Statements  //
@@ -173,4 +173,23 @@ if (birthYear <= 2000) {
   let century = 20;
 } else {
   let century = 21;
+}
+*/
+
+// ******************    //
+// CHALLENGE #2         //
+// ******************  //
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
+} else {
+  console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`);
 }
