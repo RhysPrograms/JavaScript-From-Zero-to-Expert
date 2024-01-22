d8368df55c2c1b57b8676bce95b2cfb6d2e7961c
RhysPrograms <rhysatkinson19@gmail.com>
2024-01-22 14:49:05 +0000

    UDMY-10 if / else Statements (Assignment)

--- 01 Fundamentals - Part 1/assignments.js
+++ 01 Fundamentals - Part 1/assignments.js
@@ -42,3 +42,10 @@ console.log(description);
 
 // Strings and Template Literals
 const description = `${country} is in ${continent}, and its ${population} million people speak ${language}.`;
+
+// Taking Deicisons: if / else Statements
+if (population > 33) {
+  console.log(`${country}'s population is above average.`);
+} else {
+  console.log(`${country}'s population is ${33 = population} million below the average.`);
+}
