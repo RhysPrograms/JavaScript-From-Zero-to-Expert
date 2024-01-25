24230ada7ba903dfb93a3a7fd0722b470aacafca
RhysPrograms <rhysatkinson19@gmail.com>
2024-01-25 20:21:06 +0000

    UDMY-11: Function Declarations vs. Expressions

--- 02 Fundamentals - Part 2/assignments.js
+++ 02 Fundamentals - Part 2/assignments.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*
 function describeCountry(country, population, capitalCity) {
   const choice = `${country} has ${population} million people and its capital city is ${capitalCity}`;
   return choice;
@@ -12,3 +12,19 @@ const countryPortugal = describeCountry("Portugal", 10, "Lisbon");
 console.log(describeCountry("USA", 334, "Washington, D.C"));
 console.log(describeCountry("Russia", 147, "Moscow"));
 console.log(describeCountry("China", 1409, "Beijing"));
+*/
+
+// Function Declaration
+// const age1 = calcAge1(1991); // We CAN call function declarations prior to them being defined
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+const age1 = calcAge1(1991); // We CAN call function declarations prior to them being defined
+
+// Function Expression (Function without a name, or an Anonymous Function)
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
