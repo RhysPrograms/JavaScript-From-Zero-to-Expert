2d8feeab8b5fca5662929c4547278ee971117712
RhysPrograms <rhysatkinson@protonmail.com>
2024-02-28 08:39:21 +0000

    ODIN-1: Variables & Operators (2)

--- TheOdinProject/01FoundationsCourse/JavaScript/javascript.js
+++ TheOdinProject/01FoundationsCourse/JavaScript/javascript.js
@@ -4,3 +4,7 @@ alert(admin);
 
 let ourPlanet = "Earth";
 let currentUser;
+
+const BIRTHDAY = "18.04.1982"; // make birthday uppercase? YES
+
+const age = someCode(BIRTHDAY); // make age uppercase? NO
