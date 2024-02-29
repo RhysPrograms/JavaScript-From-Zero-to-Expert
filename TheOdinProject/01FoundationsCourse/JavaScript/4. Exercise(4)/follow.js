95a54dfa1d414f37563e778ec01764715aaf345d
RhysPrograms <rhysatkinson@protonmail.com>
2024-02-29 11:36:18 +0000

    ODIN-1: Exercise(4) Complete

--- TheOdinProject/01FoundationsCourse/JavaScript/4. Exercise(4)/follow.js
+++ TheOdinProject/01FoundationsCourse/JavaScript/4. Exercise(4)/follow.js
@@ -4,23 +4,8 @@
 - If your code fails to run, or you see a "ReferenceError" in the console, review your code and make sure you have completed all of the objectives.
   
 - Don't forget you can put "console.log" anywhere to see what your values are at any time.
-  
-  ===== Step 1: =====
-  Look at the code below and make a prediction of what the output will be. 
-  
-  After making your prediction, press 'Run' at the top and look at the output in the console. If you were surprised by anything, go back and look at the code to see what's going on.
 
 */
-
-// code to be deleted
-const birthYear = 1948;
-const thisYear = 1965;
-const firstName = "Carlos";
-const lastName = "Stevenson";
-
-const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
-
-console.log(greeting);
 
 /*
 
@@ -54,58 +39,11 @@ console.log(greeting);
 
 //===== Your code goes here =================
 
+firstName = "Rhys";
+lastName = "Atkinson";
+thisYear = 2024;
+birthYear = 1998;
 
-/*
-
-  ===== Step 3: =====
-  Now that you have the code working again, let's go back over it and, using the instructions below, edit it to make it easier to read.
-
-  ---------------------------------------------------------------
-
-  1. Go to "Testing your code" below
-
-  2. Comment out the lines under "Test Step 2"
-
-  3. Uncomment the lines under "Test Step 3" (Notice the difference between them)
-
-  4. Go back to your code and create 2 new variables: "fullName" and "age"
-
-  Do NOT simply type the full name and age into the new variables. Set them using the pre-existing variables, with the calculations that are currently inside of greeting.
-
-  5. Edit the greeting string to use fullName and age instead of the other 4 variables. (the greeting should then look something like: "Hello! My name is " + fullName)
-
-  6. Press 'Run' to ensure your code still works (output should be unchanged)
-
-  ---------------------------------------------------------------
-
-  ===== Testing your code =====
-  - Do NOT edit this section until told to do so. 
-
-  - Make sure one and only one of these test steps are commented out at a time
-
-*/
-
-// Test Step 2: 
-
-module.exports = {
-  testGroup: "a",
-  greeting,
-  birthYear,
-  thisYear,
-  firstName,
-  lastName
-}
-
-
-// Test Step 3: (Don't forget to comment out lines under Test Step 2)
-
-// module.exports = {
-// 	testGroup: "b",
-// 	greeting,
-// 	birthYear,
-// 	thisYear,
-// 	firstName,
-// 	lastName,
-// 	fullName,
-// 	age
-// }
+greeting = `Hello! My name is ${firstName} ${lastName} and I am ${
+  thisYear - birthYear
+} years old.`;
