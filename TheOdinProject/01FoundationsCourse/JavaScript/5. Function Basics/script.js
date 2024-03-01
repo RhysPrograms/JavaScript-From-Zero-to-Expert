eaccb6c6d66f96d787771971efe6a76cd7b78fb6
RhysPrograms <rhysatkinson@protonmail.com>
2024-03-01 15:46:08 +0000

    ODIN-1: Function Exercise(2)

--- TheOdinProject/01FoundationsCourse/JavaScript/5. Function Basics/script.js
+++ TheOdinProject/01FoundationsCourse/JavaScript/5. Function Basics/script.js
@@ -2,4 +2,10 @@ function add7(number) {
   return number + 7;
 }
 
-console.log(add7("45"));
+console.log(add7(45));
+
+function multiply(numOne, numTwo) {
+  return numOne * numTwo;
+}
+
+console.log(multiply(9, 9));
