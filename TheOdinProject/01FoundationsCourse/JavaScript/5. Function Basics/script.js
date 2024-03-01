3c7c0f84dc686099ad64255f03977917b08682eb
RhysPrograms <rhysatkinson@protonmail.com>
2024-03-01 15:52:25 +0000

    ODIN-1: Function Exercise(3)

--- TheOdinProject/01FoundationsCourse/JavaScript/5. Function Basics/script.js
+++ TheOdinProject/01FoundationsCourse/JavaScript/5. Function Basics/script.js
@@ -1,11 +1,14 @@
 function add7(number) {
   return number + 7;
 }
-
 console.log(add7(45));
 
 function multiply(numOne, numTwo) {
   return numOne * numTwo;
 }
-
 console.log(multiply(9, 9));
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+console.log(capitalizeFirstLetter("rhys"));
