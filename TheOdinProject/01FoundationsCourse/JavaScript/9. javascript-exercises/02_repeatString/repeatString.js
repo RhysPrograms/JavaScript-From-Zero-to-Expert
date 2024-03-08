9ff42c0e99c0d95f673d52f487804785b21f38e9
RhysPrograms <rhysatkinson@protonmail.com>
2024-03-08 12:09:32 +0000

    ODIN-1: repeatString (Complete)

--- TheOdinProject/01FoundationsCourse/JavaScript/9. javascript-exercises/02_repeatString/repeatString.js
+++ TheOdinProject/01FoundationsCourse/JavaScript/9. javascript-exercises/02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (word, itr) {
+  let string = "";
+  for (let i = 0; i < itr; i++) {
+    string += word; // The += operator performs addition on the two operands and assigns the result to the left operand.
+  }
+  return string; // if this was located within the for loop, we would be given three answers, as opposed to one.
 };
 
-// Do not edit below this line
 module.exports = repeatString;
