19759227a7ed6acd95e8578c05a22abd78977671
RhysPrograms <rhysatkinson@protonmail.com>
2024-03-13 14:12:49 +0000

    ODIN-1: 04_removeFromArray. (Complete)

--- TheOdinProject/01FoundationsCourse/JavaScript/9. javascript-exercises/04_removeFromArray/removeFromArray.js
+++ TheOdinProject/01FoundationsCourse/JavaScript/9. javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (passedArray, ...args) {
+  const newArray = [];
 
+  passedArray.forEach((items) => {
+    if (!args.includes(items)) {
+      newArray.push(items);
+    }
+  });
+  return newArray;
 };
+
+// Use splice as opposed to pop as we can specify a position
 
 // Do not edit below this line
 module.exports = removeFromArray;
