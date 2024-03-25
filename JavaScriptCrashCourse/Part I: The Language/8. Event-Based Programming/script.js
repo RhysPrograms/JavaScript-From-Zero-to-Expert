abed5522cbabf1bad4c12ac5fdc63be5f351cae4
RhysPrograms <rhysatkinson@protonmail.com>
2024-03-25 11:10:40 +0000

    JSCC-8: Mouse Movement Events

--- JavaScriptCrashCourse/Part I: The Language/8. Event-Based Programming/script.js
+++ JavaScriptCrashCourse/Part I: The Language/8. Event-Based Programming/script.js
@@ -9,6 +9,7 @@ let content = document.querySelector("#main-content");
 content.addEventListener("click", () => {
   console.log("Joey doesn't share food!");
 });
+//
 
 document.querySelector("em").addEventListener("click", () => {
   console.log("You clicked on the em, element!");
@@ -22,7 +23,18 @@ let wordList = document.querySelector("#word-list");
 let sentence = document.querySelector("#sentence");
 
 wordList.addEventListener("click", (event) => {
-  let word = target.textContent;
+  let word = event.target.textContent;
   sentence.textContent += word;
   sentence.textContent += " ";
+});
+
+document.querySelector("html").addEventListener("mousemove", (e) => {
+  console.log(`mousemove x: ${e.clientX}, y: ${e.clientY}`);
+});
+
+let box = document.querySelector("#box");
+
+document.querySelector("html").addEventListener("mousemove", (e) => {
+  box.style.left = e.clientX + "px";
+  box.style.top = e.clientY + "px";
 });
