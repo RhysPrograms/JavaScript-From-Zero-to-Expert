903ab54e2468e90f32289cb3f25db7a3ade069ae
RhysPrograms <rhysatkinson@protonmail.com>
2024-03-27 09:23:27 +0000

    JSCC-9: Interacting with the Canvas

---
+++ JavaScriptCrashCourse/Part I: The Language/9. The Canvas Element/otherScript.js
@@ -0,0 +1,34 @@
+let canvas = document.querySelector("#canvas");
+let ctx = canvas.getContext("2d");
+
+let width = canvas.width;
+let height = canvas.height;
+
+let opacity = 1;
+
+function drawCircle(x, y) {
+  ctx.fillStyle = `rgba(0, 255, 0, ${opacity})`;
+  ctx.beginPath;
+  ctx.arc(x, y, 10, 0, Math.PI * 2, false);
+  ctx.fill();
+}
+
+canvas.addEventListener("click", (e) => {
+  drawCircle(e.offsetX, e.offsetY);
+});
+
+document.querySelector("#clear").addEventListener("click", () => {
+  ctx.clearRect(0, 0, width, height);
+});
+
+document.querySelector("#opacity").addEventListener("change", (e) => {
+  opacity = e.target.value;
+});
+
+// 9.4
+
+//
+
+// 9.5
+
+//
