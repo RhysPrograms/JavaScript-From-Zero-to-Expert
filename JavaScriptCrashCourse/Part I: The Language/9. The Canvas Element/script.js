b415255e239d10cbedc3448a5357e300eab854a3
RhysPrograms <rhysatkinson@protonmail.com>
2024-03-27 08:50:20 +0000

    JSCC-9: (9.1/9.2) Drawing a Shape, bigger than the Canvas

--- JavaScriptCrashCourse/Part I: The Language/9. The Canvas Element/script.js
+++ JavaScriptCrashCourse/Part I: The Language/9. The Canvas Element/script.js
@@ -1,5 +1,14 @@
 let canvas = document.querySelector("#canvas");
 let ctx = canvas.getContent("2d");
 
-ctx.fillStyle = "blue";
-ctx.fillRect(10, 10, 200, 100);
+ctx.lineWidth = 2;
+ctx.strokeStyle = "red";
+ctx.strokeRect(10, 10, 200, 100);
+
+// 9.1
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, 100, 100);
+
+// 9.2
+ctx.fillStyle = "red";
+ctx.fillRect(0, 0, 350, 350);
