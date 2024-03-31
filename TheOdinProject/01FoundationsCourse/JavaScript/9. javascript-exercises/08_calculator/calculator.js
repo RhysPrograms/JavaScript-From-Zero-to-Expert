4bfd9f4ab5e2681e4c0bc2658e26601c8391b8ac
RhysPrograms <rhysatkinson@protonmail.com>
2024-03-31 20:14:17 +0100

    ODIN-1: 08_calculator (Complete)

--- TheOdinProject/01FoundationsCourse/JavaScript/9. javascript-exercises/08_calculator/calculator.js
+++ TheOdinProject/01FoundationsCourse/JavaScript/9. javascript-exercises/08_calculator/calculator.js
@@ -1,26 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((product, current) => product * current);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
+
+const factorial = function () {};
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +36,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
