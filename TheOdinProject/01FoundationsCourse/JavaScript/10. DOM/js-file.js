b3fbfc6788452122c037a6abaabb16fe4045368f
RhysPrograms <rhysatkinson@protonmail.com>
2024-04-02 15:52:21 +0100

    ODIN-1: Adding the Elements directly to the Body, as opposed to a div

--- TheOdinProject/01FoundationsCourse/JavaScript/10. DOM/js-file.js
+++ TheOdinProject/01FoundationsCourse/JavaScript/10. DOM/js-file.js
@@ -6,7 +6,7 @@ document.querySelector("redContent");
 redContent.textContent = "Hey I'm red!";
 redContent.style.color = "red";
 
-document.getElementById("container").appendChild(redContent);
+document.body.appendChild(redContent);
 
 // Creating a Header, with blue text content
 const blueContent = document.createElement("h3");
@@ -14,13 +14,12 @@ blueContent.classList.add("blueContent");
 blueContent.textContent = "Hey I'm blue!";
 blueContent.style.color = "blue";
 
-document.getElementById("container").appendChild(blueContent);
+document.body.appendChild(blueContent);
 
 // Creating child elements within a parent container
-const otherContainer = document.createElement("div");
-const container = document.getElementById("container");
-otherContainer.classList.add("otherContainer");
-otherContainer.setAttribute("style", "border-style: solid; background: pink;");
+const container = document.createElement("div");
+container.classList.add("container");
+container.setAttribute("style", "border-style: solid; background: pink;");
 
 const divH1 = document.createElement("h1");
 divH1.classList.add("divH1");
@@ -30,10 +29,10 @@ const divP = document.createElement("p");
 divP.classList.add("divP");
 divP.textContent = "Me too!";
 
-otherContainer.appendChild(divH1);
-otherContainer.appendChild(divP);
+container.appendChild(divH1);
+container.appendChild(divP);
 
-container.appendChild(otherContainer);
+document.body.appendChild(container);
 
 // Listening for a Click
 const btn = document.querySelector("#btn");
