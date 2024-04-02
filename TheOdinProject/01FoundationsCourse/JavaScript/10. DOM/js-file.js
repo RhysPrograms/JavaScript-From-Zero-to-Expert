29df56e2fa10bb27715241692fd28262d3ba8bcf
RhysPrograms <rhysatkinson@protonmail.com>
2024-04-02 15:47:59 +0100

    ODIN-1: Adding getElementById (The JS now updates the DOM)

--- TheOdinProject/01FoundationsCourse/JavaScript/10. DOM/js-file.js
+++ TheOdinProject/01FoundationsCourse/JavaScript/10. DOM/js-file.js
@@ -1,8 +1,12 @@
 // Creating a Paragraph, with red text content
 const redContent = document.createElement("p");
 redContent.classList.add("redContent");
+
+document.querySelector("redContent");
 redContent.textContent = "Hey I'm red!";
 redContent.style.color = "red";
+
+document.getElementById("container").appendChild(redContent);
 
 // Creating a Header, with blue text content
 const blueContent = document.createElement("h3");
@@ -10,10 +14,13 @@ blueContent.classList.add("blueContent");
 blueContent.textContent = "Hey I'm blue!";
 blueContent.style.color = "blue";
 
+document.getElementById("container").appendChild(blueContent);
+
 // Creating child elements within a parent container
-const container = document.createElement("div");
-container.classList.add("container");
-container.setAttribute("style", "border-style: solid; background: pink;");
+const otherContainer = document.createElement("div");
+const container = document.getElementById("container");
+otherContainer.classList.add("otherContainer");
+otherContainer.setAttribute("style", "border-style: solid; background: pink;");
 
 const divH1 = document.createElement("h1");
 divH1.classList.add("divH1");
@@ -23,8 +30,10 @@ const divP = document.createElement("p");
 divP.classList.add("divP");
 divP.textContent = "Me too!";
 
-container.appendChild(divH1);
-container.appendChild(divP);
+otherContainer.appendChild(divH1);
+otherContainer.appendChild(divP);
+
+container.appendChild(otherContainer);
 
 // Listening for a Click
 const btn = document.querySelector("#btn");
