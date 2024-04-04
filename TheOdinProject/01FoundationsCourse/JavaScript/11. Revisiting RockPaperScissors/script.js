4c5810eaf679d2428e6da11d13d788856aa4f274
RhysPrograms <rhysatkinson@protonmail.com>
2024-04-04 10:59:41 +0100

    ODIN-1: Progress with RockPaperScissors

--- TheOdinProject/01FoundationsCourse/JavaScript/11. Revisiting RockPaperScissors/script.js
+++ TheOdinProject/01FoundationsCourse/JavaScript/11. Revisiting RockPaperScissors/script.js
@@ -1,123 +1,100 @@
-// Creating the Rock, Paper & Scissors Buttons
-// These Elements are always required, therefore I have only created them on the DOM for additional practice
-// Defining Rock
-const rock = document.createElement("button");
-rock.classList.add("rock");
-document.querySelector("rock");
-document.body.appendChild(rock);
-rock.textContent = "Rock";
-
-// Defining Paper
-const paper = document.createElement("button");
-paper.classList.add("paper");
-document.querySelector("paper");
-document.body.appendChild(paper);
-paper.textContent = "Paper";
-
-// Defining Scissors
-const scissors = document.createElement("button");
-scissors.classList.add("scissors");
-document.querySelector("scissors");
-document.body.appendChild(scissors);
-scissors.textContent = "Scissors";
-
-document.querySelector("results");
-
-// Define rock, paper & scissors
-rock = "Rock";
-paper = "Paper";
-scissors = "Scissors";
+// Retrieve References to the Buttons
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
+const result = document.getElementById("result");
+const refreshButton = document.getElementById("refreshButton");
 
 // Adding Event Listeners
-rock.addEventListener("click", () => {
-  playRound();
-});
+rockBtn.addEventListener("click", () => playRound("rock"));
+paperBtn.addEventListener("click", () => playRound("paper"));
+scissorsBtn.addEventListener("click", () => playRound("scissors"));
+refreshButton.addEventListener("click", () => location.reload());
 
-paper.addEventListener("click", () => {
-  playRound();
-});
+// Defining Scores
+let playerScore = 0;
+let computerScore = 0;
+let winningScore = 5;
 
-scissors.addEventListener("click", () => {
-  playRound();
-});
+// Getting the computers choice at random
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
+}
 
-// // Define player selection and computer selection
-// let playerSelection;
-// let computerSelection;
+// Playing a single Round
+function playRound(playerSelection) {
+  playerSelection =
+    playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+  const computerSelection = getComputerChoice();
 
-// // Define scores
-// let playerScore = 0;
-// let computerScore = 0;
+  // Determine the Winner of this Round
+  if (playerSelection === "Rock") {
+    if (computerSelection === "Scissors") {
+      playerScore++;
+      result.textContent = `Result: You Win! ${playerSelection} beats ${computerSelection}`;
+    } else if (computerSelection === "Paper") {
+      computerScore++;
+      result.textContent = `Result: You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+      result.textContent = "Tie Game!";
+    }
+  } else if (playerSelection === "Paper") {
+    if (computerSelection === "Rock") {
+      playerScore++;
+      result.textContent = `Result: You Win! ${playerSelection} beats ${computerSelection}`;
+    } else if (computerSelection === "Scissors") {
+      computerScore++;
+      result.textContent = `Result: You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+      result.textContent = "Tie Game!";
+    }
+  } else if (playerSelection === "Scissors") {
+    if (computerSelection === "Paper") {
+      playerScore++;
+      result.textContent = `Result: You Win! ${playerSelection} beats ${computerSelection}`;
+    } else if (computerSelection === "Rock") {
+      computerScore++;
+      result.textContent = `Result: You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+      result.textContent = "Tie Game!";
+    }
+  } else {
+    alert("Invalid Input: Please choose between Rock, Paper, and Scissors.");
+  }
 
-// // Get the computers choice at random
-// function getComputerChoice(rock, paper, scissors) {
-//   const choices = [rock, paper, scissors];
-//   return choices[Math.floor(Math.random() * choices.length)];
-// }
+  if (playerScore === winningScore || computerScore === winningScore) {
+    declareWinner();
+  }
+}
 
-// function playRound(playerSelection, computerSelection) {
-//   playerSelection =
-//     playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
-//   const computerSelection = getComputerChoice(rock, paper, scissors);
+// Function to Declare the Winner
+function declareWinner() {
+  if (playerScore > computerScore) {
+    console.log(
+      "You win with a score of " +
+        playerScore +
+        " and the computer had: " +
+        computerScore
+    );
+  } else if (computerScore > playerScore) {
+    console.log(
+      "You lose! The computers score was " +
+        computerScore +
+        " and you had " +
+        playerScore
+    );
+  } else {
+    console.log(
+      "It's a tie! With an equal score of " +
+        playerScore +
+        " to " +
+        computerScore
+    );
+  }
 
-//   if (playerSelection == "Rock") {
-//     if (computerSelection == "Scissors")
-//       return (
-//         playerScore++,
-//         (results.textContent = `Result: You Win! ${playerSelection} beats ${computerSelection}`)
-//       );
-//     else if (computerSelection == "Paper")
-//       return (
-//         computerScore++,
-//         (results.textContent = `Result: You Lose! ${computerSelection} beats ${playerSelection}`)
-//       );
-//     else return "Tie Game!";
-//   } else if (playerSelection == "Paper") {
-//     if (computerSelection == "Rock")
-//       return (
-//         playerScore++,
-//         (results.textContent = `Result: You Win! ${playerSelection} beats ${computerSelection}`)
-//       );
-//     else if (computerSelection == "Scissors")
-//       return (
-//         computerScore++,
-//         (results.textContent = `Result: You Lose! ${computerSelection} beats ${playerSelection}`)
-//       );
-//     else return "Tie Game!";
-//   } else if (playerSelection == "Scissors") {
-//     if (computerSelection == "Paper")
-//       return (
-//         playerScore++,
-//         (results.textContent = `Result: You Win ${playerSelection} beats ${computerSelection}`)
-//       );
-//     else if (computerSelection == "Rock")
-//       return (
-//         computerScore++,
-//         (results.textContent = `Result: You Lose! ${computerSelection} beats ${playerSelection}`)
-//       );
-//     else return "Tie Game!";
-//   } else
-//     return alert(
-//       "Invalid Input: Please choose between Rock, Paper, and Scissors."
-//     );
-// }
-// // Loop playRound 5 times and determine the winner
-// // function playGame() {
-// //   for (let rounds = 0; rounds < 5; rounds++) {
-// //     console.log(playRound(playerSelection, computerSelection));
-// //     console.log('Your score: ' + playerScore);
-// //     console.log('Computer score: ' + computerScore);
-// //     console.log('----------------------------------')
-// //   }
-// //   if (playerScore > computerScore) {
-// //     console.log('You win with a score of ' + playerScore + ' and the computer had: ' + computerScore);
-// //   }
-// //   else if (computerScore > playerScore) {
-// //     console.log('You lose! ' + 'The computers score was ' + computerScore + ' and you had ' + playerScore);
-// //   }
-// //   else {
-// //     console.log('Its a tie! With an equal score of ' + playerScore + ' to ' + computerScore);
-// //   }
-// // }
-
-// // playGame();
+  // Disable the Buttons, after the Game
+  rockBtn.disabled = true;
+  paperBtn.disabled = true;
+  scissorsBtn.disabled = true;
+}
