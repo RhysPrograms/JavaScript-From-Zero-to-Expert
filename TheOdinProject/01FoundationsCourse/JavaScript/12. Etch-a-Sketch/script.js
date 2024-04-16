8a865085b8d5ee8be364db492cda119f7bd3aad6
RhysPrograms <rhysatkinson@protonmail.com>
2024-04-16 09:22:12 +0100

    ODIN-12: Working Etch-a-Sketch

--- TheOdinProject/01FoundationsCourse/JavaScript/12. Etch-a-Sketch/script.js
+++ TheOdinProject/01FoundationsCourse/JavaScript/12. Etch-a-Sketch/script.js
@@ -1,14 +1,25 @@
 // Retrieving the reference to the Container
 const container = document.getElementById("container");
 
-const numGridElements = 16;
+const numGridElements = 24;
+let isDrawing = false;
 
 for (let i = 0; i < numGridElements; i++) {
-  // Creating the gridSquare, element
+  // Creating the gridSquare element
   const gridSquare = document.createElement("div");
-  // Appending 'gridSquare' to it's parent element 'container'
+  // Appending 'gridSquare' to its parent element 'container'
   container.appendChild(gridSquare);
   // Adding CSS
   gridSquare.classList.add("gridSquare");
   gridSquare.style.cssText = "border: 1px solid #ccc; flex: 1 0 25%;";
+  // Add event listener for mouseover
+  gridSquare.addEventListener("mouseover", () => draw(gridSquare));
 }
+
+// Function to handle drawing
+function draw(square) {
+  square.style.backgroundColor = "black";
+}
+
+// Event listener for mouseover to control drawing
+document.addEventListener("mouseover", () => (isDrawing = true));
