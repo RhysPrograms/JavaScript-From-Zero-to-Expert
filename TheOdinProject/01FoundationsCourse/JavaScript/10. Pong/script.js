8b63ddec1d9474eac24cf47d8fb165a01fbb42af
RhysPrograms <rhysatkinson@protonmail.com>
2024-05-03 14:06:10 +0100

    JSCC-11: Completing the Game Class

--- TheOdinProject/01FoundationsCourse/JavaScript/10. Pong/script.js
+++ TheOdinProject/01FoundationsCourse/JavaScript/10. Pong/script.js
@@ -190,4 +190,41 @@ class Game {
     this.gameView.draw(this.ball, this.leftPaddle, this.rightPaddle);
     this.gameView.drawScores(this.scores);
   }
+
+  checkCollision() {
+    this.ball.checkPaddleCollision(this.leftPaddle, Math.abs(this.ball.xSpeed));
+    this.ball.checkPaddleCollision(
+      this.rightPaddle,
+      -Math.abs(this.ball.xSpeed)
+    );
+
+    this.ball.checkWallCollision(
+      this.gameView.width,
+      this.gameView.height,
+      this.scores
+    );
+
+    if (this.scores.leftScore > 9 || this.scores.rightScpre > 9) {
+      this.gameOver = true;
+    }
+  }
+
+  update() {
+    this.ball.update();
+    Computer.followBall(this.leftPaddle, this.ball);
+  }
+
+  loop() {
+    this.draw();
+    this.update();
+    this.checkCollision();
+
+    if (this.gameOver) {
+      this.draw();
+      this.gameView.drawGameOver();
+    } else {
+      // Call this method again after a Timeout
+      setTimeout(() => this.loop(), 30);
+    }
+  }
 }
