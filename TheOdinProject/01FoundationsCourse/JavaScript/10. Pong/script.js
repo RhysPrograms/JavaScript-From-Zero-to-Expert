1217e2893cc4590d80d88006df77f11d61bb20d5
RhysPrograms <rhysatkinson@protonmail.com>
2024-05-03 13:00:03 +0100

    JSCC-11: The GameView Class

--- TheOdinProject/01FoundationsCourse/JavaScript/10. Pong/script.js
+++ TheOdinProject/01FoundationsCourse/JavaScript/10. Pong/script.js
@@ -1,187 +1,39 @@
-let canvas = document.querySelector("#canvas");
-let ctx = canvas.getContext("2d");
-let width = canvas.width;
-let height = canvas.height;
+// `this` refers to the current instance of the GameView class.
+//  this.ctx, this.width, etc, are properties of the current instance of the GameView class.
+class GameView {
+  constructor() {
+    let canvas = document.querySelector("#canvas");
+    this.ctx = canvas.getContext("2d");
+    this.width = canvas.width;
+    this.height = canvas.height;
+    this.offsetTop = canvas.offsetTop;
+  }
 
-const MAX_COMPUTER_SPEED = 2;
+  // Rest parameters (indicated by three dots (...)) followed by the parameter name
+  // allow you to represent an indefinite number of arguments as an array.
+  draw(...entities) {
+    // Fill the Canvas with Black
+    this.ctx.fillStyle = "black";
+    this.ctx.fillRect(0, 0, this.width, this.height);
 
-const BALL_SIZE = 5; // True Constant, all-caps style
-let ballPosition;
+    entities.forEach((entity) => entity.draw(this.ctx));
+  }
 
-let xSpeed;
-let ySpeed;
+  drawScores(scores) {
+    this.ctx.fillStyle = "white";
+    this.ctx.font = "30px monospace";
 
-function initBall() {
-  ballPosition = { x: 20, y: 30 };
-  xSpeed = 4;
-  ySpeed = 2;
-}
+    this.ctx.textAlign = "left";
+    this.ctx.fillText(scores.leftScore.toString(), 50, 50);
 
-const PADDLE_WIDTH = 5;
-const PADDLE_HEIGHT = 20;
-const PADDLE_OFFSET = 10;
+    this.ctx.textAlign = "right";
+    this.ctx.fillText(scores.rightScore.toString(), -50, 50);
+  }
 
-let leftPaddleTop = 10;
-let rightPaddleTop = 30;
-
-let leftScore = 0;
-let rightScore = 0;
-let gameOver = false;
-
-document.addEventListener("mousemove", (e) => {
-  rightPaddleTop = e.y - canvas.offsetTop;
-});
-
-function draw() {
-  // Fill the canvas with black
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, width, height);
-
-  // All remaining elements will be white
-  ctx.fillStyle = "white";
-
-  // Drawing the ball
-  ctx.fillRect(ballPosition.x, ballPosition.y, BALL_SIZE, BALL_SIZE);
-
-  // Drawing the Paddles
-  ctx.fillRect(PADDLE_OFFSET, leftPaddleTop, PADDLE_WIDTH, PADDLE_HEIGHT);
-  ctx.fillRect(
-    width - PADDLE_WIDTH - PADDLE_OFFSET,
-    rightPaddleTop,
-    PADDLE_WIDTH,
-    PADDLE_HEIGHT
-  );
-
-  // Drawing the Scores
-  ctx.font = "30px monospace";
-  ctx.textAlign = "left";
-  ctx.fillText(leftScore.toString(), 50, 50);
-  ctx.textAlign = "right";
-  ctx.fillText(rightScore.toString(), width - 50, 50);
-}
-
-function followBall() {
-  let ball = {
-    top: ballPosition.y,
-    bottom: ballPosition.y + BALL_SIZE,
-  };
-  let leftPaddle = {
-    top: leftPaddleTop,
-    bottom: leftPaddleTop + PADDLE_HEIGHT,
-  };
-
-  if (ball.top < leftPaddle.top) {
-    leftPaddleTop -= MAX_COMPUTER_SPEED;
-  } else if (ball.bottom > leftPaddle.bottom) {
-    leftPaddleTop += MAX_COMPUTER_SPEED;
+  drawGameOver() {
+    this.ctx.fillStyle = "white";
+    this.ctx.font = "30px monospace";
+    this.ctx.textAlign = "center";
+    this.text.fillText("GAME OVER", this.width / 2, this.height / 2);
   }
 }
-
-function update() {
-  ballPosition.x += xSpeed;
-  ballPosition.y += ySpeed;
-  followBall();
-}
-
-function checkPaddleCollision(ball, paddle) {
-  // Confirm whether or not the paddle and the ball overlap
-  return (
-    ball.left < paddle.right &&
-    ball.right > paddle.left &&
-    ball.top < paddle.bottom &&
-    ball.bottom > paddle.top
-  );
-}
-
-function adjustAngle(distanceFromTop, distanceFromBottom) {
-  console.log(`Top: ${distanceFromTop}, Bottom: ${distanceFromBottom}`);
-  if (distanceFromTop < 2) {
-    // If ball is struck near the top of the paddle, reduce ySpeed
-    console.log(`Top Hit!`);
-    ySpeed -= 0.5;
-  }
-  if (distanceFromBottom < 2) {
-    // If ball is struck near the bottom of the paddle, increase ySpeed
-    console.log(`Bottom Hit!`);
-    ySpeed += 0.5;
-  }
-}
-
-function checkCollision() {
-  let ball = {
-    left: ballPosition.x,
-    right: ballPosition.x + BALL_SIZE,
-    top: ballPosition.y,
-    bottom: ballPosition.y + BALL_SIZE,
-  };
-
-  let leftPaddle = {
-    left: PADDLE_OFFSET,
-    right: PADDLE_OFFSET + PADDLE_WIDTH,
-    top: leftPaddleTop,
-    bottom: leftPaddleTop + PADDLE_HEIGHT,
-  };
-
-  let rightPaddle = {
-    left: width - PADDLE_WIDTH - PADDLE_OFFSET,
-    right: width - PADDLE_OFFSET,
-    top: rightPaddleTop,
-    bottom: rightPaddleTop + PADDLE_HEIGHT,
-  };
-
-  if (checkPaddleCollision(ball, leftPaddle)) {
-    // if Left Paddle Collision
-    let distanceFromTop = ball.top - leftPaddle.top;
-    let distanceFromBottom = leftPaddle.bottom - ball.bottom;
-    adjustAngle(distanceFromTop, distanceFromBottom);
-    xSpeed = Math.abs(xSpeed);
-  }
-
-  if (checkPaddleCollision(ball, rightPaddle)) {
-    // if Right Paddle Collision
-    let distanceFromTop = ball.top - rightPaddle.top;
-    let distanceFromBottom = rightPaddle.bottom - ball.bottom;
-    adjustAngle(distanceFromTop, distanceFromBottom);
-    xSpeed = -Math.abs(xSpeed);
-  }
-
-  if (ball.left < 0) {
-    rightScore++;
-    initBall();
-  }
-
-  if (ball.right > width) {
-    leftScore++;
-    initBall();
-  }
-
-  if (leftScore > 9 || rightScore > 9) {
-    gameOver = true;
-  }
-
-  if (ball.top < 0 || ball.bottom > height) {
-    ySpeed = -ySpeed;
-  }
-}
-
-function drawGameOver() {
-  ctx.fillStyle = "white";
-  ctx.font = "30px monospace";
-  ctx.textAlign = "center";
-  ctx.fillText("GAME OVER", width / 2, height / 2);
-}
-
-function gameLoop() {
-  draw();
-  update();
-  checkCollision();
-  if (gameOver) {
-    draw();
-    drawGameOver();
-  } else {
-    setTimeout(gameLoop, 25);
-  }
-}
-
-initBall();
-gameLoop();
